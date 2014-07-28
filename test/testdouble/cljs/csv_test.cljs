eb0a544b478fc1126adeca22156b3d6cffe328b3
Matthew Boston <matthew.boston@gmail.com>
2014-07-28 11:12:44 -0600

    add a test for user defined separator

--- test/testdouble/cljs/csv_test.cljs
+++ test/testdouble/cljs/csv_test.cljs
@@ -1,7 +1,12 @@
 (ns testdouble.cljs.csv-test
   (:require [testdouble.cljs.csv :as csv]
             [cemerick.cljs.test :as t])
-  (:require-macros [cemerick.cljs.test :refer [deftest is]]))
+  (:require-macros [cemerick.cljs.test :refer [deftest testing is]]))
 
 (deftest write-csv-test
-  (is (= "1,2,3\n4,5,6" (csv/write-csv [[1 2 3] [4 5 6]]))))
+  (let [data [[1 2 3] [4 5 6]]]
+    (testing "default separator ','"
+      (is (= "1,2,3\n4,5,6" (csv/write-csv data))))
+
+    (testing "user defined separator '|'"
+      (is (= "1|2|3\n4|5|6" (csv/write-csv data :separator "|"))))))
