53017ff26eba4003457f8fc9e0b77cb75f4c7f2d
Matthew Boston <matthew.boston@gmail.com>
2014-09-15 11:30:26 -0600

    upgrade to ClojureScript 0.0-2322

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/clojurescript "0.0-2322"]
                  [com.cemerick/clojurescript.test "0.3.1"]]
   :plugins [[lein-cljsbuild "1.0.3"]
             [com.cemerick/clojurescript.test "0.3.1" :scope "test"]
