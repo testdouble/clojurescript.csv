383ce5176b19819f0ce362d57ca73e0a1a982863
Matthew Boston <matthew.boston@gmail.com>
2015-09-22 17:35:57 -0600

    upgrade cljsbuild 1.1.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2511"]]
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.1.0"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds [{:id "whitespace"
                         :source-paths ["src" "test"]
