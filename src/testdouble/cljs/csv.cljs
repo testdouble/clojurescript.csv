a235ff9fed1744ad76ed89c533809ada6d056309
Matthew Boston <matthew@matthewboston.com>
2018-06-11 18:06:35 -0400

    fixes #6 - misspelled function

--- src/testdouble/cljs/csv.cljs
+++ src/testdouble/cljs/csv.cljs
@@ -4,13 +4,13 @@
 (defn- wrap-in-quotes [s]
   (str "\"" s "\""))
 
-(defn- seperate [data separator quote?]
+(defn- separate [data separator quote?]
   (str/join separator
             (cond->> data
               quote? (map wrap-in-quotes))))
 
 (defn- write-data [data separator newline quote?]
-  (str/join newline (map #(seperate % separator quote?) data)))
+  (str/join newline (map #(separate % separator quote?) data)))
 
 (def ^:private newlines
   {:lf "\n" :cr+lf "\r\n"})
