f47ac922fdea6dd9b56bbe202cb7422a0d81785d
rosston <ross@testdouble.com>
2018-11-14 19:41:38 -0500

    Add failing tests for RFC 4180 compliance

--- test/testdouble/cljs/csv_test.cljs
+++ test/testdouble/cljs/csv_test.cljs
@@ -22,6 +22,10 @@
     (testing "quote each field"
       (is (= "\"1,000\",\"2\",\"3\"\n\"4\",\"5,000\",\"6\"" (csv/write-csv [["1,000" "2" "3"] ["4" "5,000" "6"]] :quote? true))))
 
+    (testing "valid characters in quoted fields"
+      (is (= "\"a\nb\",\"c\rd\"\n\"e,f\",\"g\"\"h\""
+             (csv/write-csv [["a\nb" "c\rd"] ["e,f" "g\"h"]] :quote? true))))
+
     (testing "error when newline is not one of :lf OR :cr+lf"
       (is (thrown-with-msg? js/Error #":newline" (csv/write-csv data :newline "foo"))))))
 
@@ -37,7 +41,11 @@
       (is (= data (csv/read-csv "1,2,3\r\n4,5,6" :newline :cr+lf))))
 
     (testing "user defined separator '|' and newline ':cr+lf'"
-      (is (= data (csv/read-csv "1|2|3\r\n4|5|6" :separator "|" :newline :cr+lf))))))
+      (is (= data (csv/read-csv "1|2|3\r\n4|5|6" :separator "|" :newline :cr+lf))))
+
+    (testing "valid characters in quoted fields"
+      (is (= [["a\nb" "c\rd"] ["e,f" "g\"h"]]
+             (csv/read-csv "\"a\nb\",\"c\rd\"\n\"e,f\",\"g\"\"h\""))))))
 
 (defn ^:export run []
   (run-tests))
