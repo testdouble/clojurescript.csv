f5493dd86ffb386e71e43ca6e9c3f9239bc38cfe
Ross Brandes <ross@testdouble.com>
2019-11-06 10:29:53 -0500

    0.4.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject testdouble/clojurescript.csv "0.4.5"
+(defproject testdouble/clojurescript.csv "0.4.6-SNAPSHOT"
   :description "A ClojureScript library for reading and writing comma (and other) separated values."
   :url "https://github.com/testdouble/clojurescript.csv"
   :license {:name "Eclipse Public License"
